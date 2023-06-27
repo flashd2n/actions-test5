63ab5c80e463535e3902ce328df04e590bd56243
Kalin Kostov <k.kostov.eu@gmail.com>
2023-06-27 23:27:15 +0300

    asd
    Signed-off-by: Kalin Kostov <k.kostov.eu@gmail.com>

---
+++ .github/actions/deploy-s3-docker/src/index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+const run = async () => {
+
+    const bucket = process.env.INPUT_BUCKET;
+    const bucketRegion = process.env.INPUT_BUCKET-REGION;
+    const distFolder = process.env.INPUT_DIST-FOLDER;
+
+    console.log("does it work?")
+    console.log(bucket);
+    console.log(bucketRegion);
+    console.log(distFolder);
+
+    const websiteUrl = "ulalla";
+
+    console.log(`::set-output name=website-url::${websiteUrl}`);
+
+};
+
+run();
--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -2,7 +2,16 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-const run = () => {
+const run = async () => {
+    const bucket = core.getInput("bucket", { required: true });
+    const bucketRegion = core.getInput("bucket-region", { required: true });
+    const distFolder = core.getInput("dist-folder", { required: true });
+
+    await exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion} --delete`);
+
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput("website-url", websiteUrl);
+
     core.notice("hello from the custom JS action");
 };
 
