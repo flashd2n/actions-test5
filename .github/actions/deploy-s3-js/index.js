ff75e27195106aa8a0e640fbc6edb2b9c4eb9a8e
Kalin Kostov <k.kostov.eu@gmail.com>
2023-07-01 19:07:16 +0300

    asd
    Signed-off-by: Kalin Kostov <k.kostov.eu@gmail.com>

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -6,8 +6,13 @@ const run = async () => {
     const bucket = core.getInput("bucket", { required: true });
     const bucketRegion = core.getInput("bucket-region", { required: true });
     const distFolder = core.getInput("dist-folder", { required: true });
+    const collection = core.getInput("collection", { required: true });
 
-    await exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion} --delete`);
+    console.log(collection);
+    console.log(typeof collection);
+    console.log(JSON.parse(collection));
+
+    // await exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion} --delete`);
 
     const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
     core.setOutput("website-url", websiteUrl);
